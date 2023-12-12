e216606dbf42c0b1b87bff7b9ab3497331d96941
Alejandro <ajosuemendez@gmail.com>
2023-12-12 17:13:45 +0100

    removed click event logic to send single square id

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -19,44 +19,13 @@ export default function Board() {
     const handleClick = async (e) => {
         const clickedSquare = e.target.id;
 
-        if (selectedOriginSquare === "") {
-            
-            console.log(`Making API call "Check Valid Moves": selected origin: ${clickedSquare}`);
-            console.log("API response......");
-            setSelectedOriginSquare(clickedSquare);
-            console.log("new Chessboard state with valid moves");
-            setChessboardState(newChessBoardState);
-            console.log("ERROR?:", errorNewChessBoardState);
-            
-            // if response comes without valid moves then the player has chosen a wrong piece or none.
-            const responseWithoutValidMoves = false;
-            if (responseWithoutValidMoves) {
-                console.log("Getting new chessboard state without Valid Moves from the API");
-                return;
-            }
-
-            console.log("Getting new chessboard state WITH Valid Moves from the API");
-            console.log(`Setting selected origin to ${clickedSquare}`);
-            setSelectedOriginSquare(clickedSquare);
-            return;
-        }
-
-        console.log(`Making API call "Move Piece": selected origin: ${selectedOriginSquare} , selected destination: ${clickedSquare}`);
-        console.log("API response....");
-
-        // If getting a chessboard state with valid moves then we have clicked another piece of us
-        const responseWithValidMoves = false;
-        if (responseWithValidMoves) {
-            console.log("Getting new chessboard state WITH valid moves from the API");
-            console.log(`Setting selected origin to ${clickedSquare}`);
-            selectedOriginSquare(clickedSquare);
-            return;
-        }
-        
-        // If getting a chessboard state without any valid moves then we updated the last played and now is the next players turn
-        console.log("Getting new chessboard state WITH valid moves from the API");
-        console.log(`Setting selected origin to ""`);
-        setSelectedOriginSquare("");
+        console.log(`Making API call "Check Valid Moves": selected origin: ${clickedSquare}`);
+        console.log("API response......");
+        setSelectedOriginSquare(clickedSquare);
+        console.log("new Chessboard state with valid moves");
+        // setChessboardState(newChessBoardState);
+        console.log(newChessBoardState);
+        console.log("ERROR?:", errorNewChessBoardState);
 
         return;
     }
--- frontend/src/components/Piece.js
+++ frontend/src/components/Piece.js
@@ -1,13 +1,11 @@
 
-export default function Piece({key, pieceType, theme, framePosition}) {
+export default function Piece({pieceType, theme, framePosition}) {
     
     if ( pieceType === null ) {
         return <></>;
     }
 
     const pieceImage = process.env.PUBLIC_URL + `/images/themes/${theme}/${pieceType}-${theme}.png`;
-
-    console.log(key);
 
     return <image   key={pieceType}
                     href={pieceImage}
