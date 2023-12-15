0e7982a0def3b34f0956b77c7daee2f9536c5f68
isha <isha.suryavanshi@st.ovgu.de>
2023-12-15 18:32:50 +0100

    KnightMoves

--- backend/InputHandlerService/src/main/java/com/thechessnuts/InputHandlerService/models/Main.java
+++ backend/InputHandlerService/src/main/java/com/thechessnuts/InputHandlerService/models/Main.java
@@ -1,11 +1,17 @@
 package com.thechessnuts.InputHandlerService.models;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Game g = new Game();
+       /*  Game g = new Game();
         g.setPlayers(new Player("PLayer-One", "WHITE"), new Player("Player-Two", "BLACK"),  new Player("Player-Three", "YELLOW"));
         g.newGame();
         g.board.selectPiece("h11");
-        return;
+        return;*/
+        Chess3Board board=new Chess3Board();
+        board.findSquare("d6").setPiece(new Knight(board,new Player()));
+        ArrayList<Square> list=board.findSquare("d6").piece.allMoves();
+        System.out.println(list);
     }
 }
