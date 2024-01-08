95fdc03ae5c8bc8775d4f5c6a37a2848f47e0004
Alejandro <ajosuemendez@gmail.com>
2024-01-08 12:11:14 +0100

    created Routes for Home Page and Not found Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 // import './App.css';
 
-import Board from "./components/Board";
+// import Board from "./components/Board";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from "./pages/Home";
+import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
   return(
-    <Board/>
+    <Router>
+      {/* <Header /> */}
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="*" element={<NotFoundPage/>} />
+      </Routes>
+    </Router>
   );
-}
+};
