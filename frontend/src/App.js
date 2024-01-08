1aa86626d2e52d26465128d5fd5868e1f2b8237c
Alejandro <ajosuemendez@gmail.com>
2024-01-08 15:26:38 +0100

    added Game page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Header from './layout/Header';
 
 // Pages
 import Home from "./pages/Home";
+import Game from "./pages/Game";
 import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
       <Header/>
       <Routes>
         <Route exact path="/" element={<Home/>} />
+        <Route exact path="/game" element={<Game/>} />
         <Route path="*" element={<NotFoundPage/>} />
       </Routes>
     </Router>
