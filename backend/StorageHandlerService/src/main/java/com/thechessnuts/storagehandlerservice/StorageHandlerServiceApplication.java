7a37a5ba994a82019ff4725f55e7edf65266e54b
Alejandro <ajosuemendez@gmail.com>
2024-01-27 17:22:28 +0100

    added test endpoint for api gateway

--- backend/StorageHandlerService/src/main/java/com/thechessnuts/storagehandlerservice/StorageHandlerServiceApplication.java
+++ backend/StorageHandlerService/src/main/java/com/thechessnuts/storagehandlerservice/StorageHandlerServiceApplication.java
@@ -2,8 +2,11 @@ package com.thechessnuts.storagehandlerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StorageHandlerServiceApplication {
 
     public static void main(String[] args) {
