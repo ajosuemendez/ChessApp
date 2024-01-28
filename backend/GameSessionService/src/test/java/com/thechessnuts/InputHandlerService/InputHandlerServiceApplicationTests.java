49e69b69c30b7cc25438db6a49ac397b008e7e8e
janakan <136900953+jling1234@users.noreply.github.com>
2024-01-28 16:54:57 +0100

    Removed InputHandlerServiceApplicationTests.java

--- backend/GameSessionService/src/test/java/com/thechessnuts/InputHandlerService/InputHandlerServiceApplicationTests.java
+++ backend/GameSessionService/src/test/java/com/thechessnuts/InputHandlerService/InputHandlerServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.thechessnuts.InputHandlerService;
+/*package com.thechessnuts.InputHandlerService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class InputHandlerServiceApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
